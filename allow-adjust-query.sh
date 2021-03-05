91d2af2dedc7862db9395e480a9eda65d5943057
Ben Burdick <ben@opsani.com>
2021-03-04 17:25:27 -0800

    add blackout window support

---
+++ allow-adjust-query.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+APPLICATION=$1
+URL=$2
+RESULTS=`curl -sS -H 'Content-type: application/json' -H 'Accept: application/json' -X POST -k ${URL}/${APPLICATION} -d '{"deployType": "Harness","desc": "opsani-servo blackout window check", "noOfHours": 1}'`
+RESULTS=`/bin/cat output.txt`
+APPROVED=`echo $RESULTS | jq .approved`
+MESSAGE=`echo $RESULTS | jq .message`
+
+if [ "$APPROVED" == '"true"' ]; then
+  echo $MESSAGE
+  exit 0
+else
+  echo $MESSAGE
+  exit 1
+fi
